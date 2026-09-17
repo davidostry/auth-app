2234a2044e5d593eda151436a5b3f4dc05cce2a4
david <sarigoo421@gmail.com.il>
2026-09-17 09:41:48 +0300

    added basic dal

---
+++ backend/DAL/users.js
@@ -0,0 +1,19 @@
+import { db } from '..DB/config.js';
+
+const users = db.collection("users");
+
+export async function createUser(userName, email, hash) {
+
+  const { insertedId } = await users.insertOne({ userName, email, hash })
+  return insertedId
+}
+
+export async function findUser(email) {
+  const correctedEmail = email.toLowerCase();
+  const user = await users.findOne({ email: correctedEmail });
+  return user;
+}
+
+export async function getAll() {
+  return await users.find().toArray()
+}
